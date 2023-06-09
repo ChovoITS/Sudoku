99d862f34837861eb10e0fe837e10e5000169e5f
s.carobene@itsrizzoli.it <115108642+stecno04@users.noreply.github.com>
2023-06-09 15:36:44 +0200

    Aggiunta numero massimo suggerimenti

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from colorama import Fore, Style
+import time
 
 sudoku_matrix = [[5,3,0,0,7,0,0,0,0],
                  [6,0,0,1,9,5,0,0,0],
@@ -9,6 +10,9 @@ sudoku_matrix = [[5,3,0,0,7,0,0,0,0],
                  [0,6,0,0,0,0,2,8,0],
                  [0,0,0,4,1,9,0,0,5],
                  [0,0,0,0,8,0,0,7,9]]
+
+suggestion_n = 3
+waiting_time = 5
 
 def print_sudoku():
     print("\n")
@@ -35,6 +39,7 @@ def insert_number(column:int, row:int, value:int):
     print_sudoku()
 
 def get_suggestion():
+       
     solution_matrix = [[5,3,4,6,7,8,9,1,2],
                       [6,7,2,1,9,5,3,4,8],
                       [1,9,8,3,4,2,5,6,7],
@@ -44,14 +49,41 @@ def get_suggestion():
                       [9,6,1,5,3,7,2,8,4],
                       [2,8,7,4,1,9,6,3,5],
                       [2,4,5,2,8,6,1,7,9]]
-    row = int(input("Il numero che vorresti conoscere in che riga si trova? "))
-    column = int(input("Il numero che vorresti conoscere in che colonna si trova? "))
-    if(sudoku_matrix[row-1][column-1] != 0):
-        print(Fore.RED + 'Il numero richiesto è già pieno')
-        print(Style.RESET_ALL, end="")
+    if(suggestion_n==0):
+        print(f"Hai finito i suggerimenti disponibili, se te ne serve un altro dovrai aspettare {waiting_time} secondi. ")
+        k = input("vuoi ancora il suggerimento o no? Y/N: ")
+        if(k==('N' or 'n')):
+            print("Ottima scelta!")
+        else:
+            print("Ti consiglio un caffè nell'attesa! ")
+            time.sleep(waiting_time)
+            
+            row = int(input("Il numero che vorresti conoscere in che riga si trova? "))
+            column = int(input("Il numero che vorresti conoscere in che colonna si trova? "))
+            while(sudoku_matrix[row-1][column-1] != 0):
+                if(sudoku_matrix[row-1][column-1] != 0):
+                    print(Fore.RED + 'Il numero richiesto è già pieno, riprova!')
+                    print(Style.RESET_ALL, end="")
+                    
+                    row = int(input("Il numero che vorresti conoscere in che riga si trova? "))
+                    column = int(input("Il numero che vorresti conoscere in che colonna si trova? "))
+            print(f'Nella casella {row, column} il numero da mettere è {solution_matrix[row-1][column-1]}')
+    
     else:
+        row = int(input("Il numero che vorresti conoscere in che riga si trova? "))
+        column = int(input("Il numero che vorresti conoscere in che colonna si trova? "))
+        while(sudoku_matrix[row-1][column-1] != 0):
+            if(sudoku_matrix[row-1][column-1] != 0):
+                print(Fore.RED + 'Il numero richiesto è già pieno, riprova!')
+                print(Style.RESET_ALL, end="")
+                
+                row = int(input("Il numero che vorresti conoscere in che riga si trova? "))
+                column = int(input("Il numero che vorresti conoscere in che colonna si trova? "))
         print(f'Nella casella {row, column} il numero da mettere è {solution_matrix[row-1][column-1]}')
+    
+    return suggestion_n-1
 
+            
 while True:
     try:
         answer = int(input("Menu \n \n1. Visualizza il sudoku \n2. Inserisci un numero \n3. Chiedi un suggerimento \n4. Exit \n"))
@@ -78,7 +110,7 @@ while True:
                     break
             insert_number(column, row, value)
         case 3:
-            get_suggestion()
+            suggestion_n = get_suggestion()
         case 4:
             break
         case _:
