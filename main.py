799e1923b3cd9d29e63f76a47fe9634a3202700a
s.carobene@itsrizzoli.it <115108642+stecno04@users.noreply.github.com>
2023-06-09 14:25:54 +0200

    Creazione della funzione suggerimento

--- main.py
+++ main.py
@@ -15,6 +15,22 @@ def insert_number():
     return 0
 
 def get_suggestion():
+    solution_matrix = [[5,3,4,6,7,8,9,1,2],
+                      [6,7,2,1,9,5,3,4,8],
+                      [1,9,8,3,4,2,5,6,7],
+                      [8,5,8,7,6,1,4,2,3],
+                      [4,2,6,8,5,3,7,9,1],
+                      [7,1,3,9,2,4,8,5,6],
+                      [9,6,1,5,3,7,2,8,4],
+                      [2,8,7,4,1,9,6,3,5],
+                      [2,4,5,2,8,6,1,7,9]]
+    row = int(input("Il numero che vorresti conoscere in che riga si trova? "))
+    column = int(input("Il numero che vorresti conoscere in che colonna si trova? "))
+    if(sudoku_matrix[row-1][column-1] != 0):
+        print('Il numero richiesto è già pieno')
+    else:
+        print(f'Nella casella {row, column} il numero da mettere è {solution_matrix[row-1][column-1]}')
+
     return 0
 
 while True:
